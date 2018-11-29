648390678f2bae276b4f1b5d5c11c14ecac3f86d
Besiddes <Besiddes1@gmail.com>
2018-11-28 20:56:18 -0600

    Creacion de rutas en NavBar principal

--- SecurityHome/src/app/app-routing.module.ts
+++ SecurityHome/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomePageComponent} from './componentes/home-page/home-page.component';
+import {LoginComponent} from './componentes/login/login.component';
+import {RegistroComponent} from './componentes/registro/registro.component';
+import {PrivadoComponent} from './componentes/privado/privado.component';
+import {NotFoundComponent} from './componentes/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',component:HomePageComponent},
+  {path: 'login',component: LoginComponent},
+  {path: 'Registro',component:RegistroComponent},
+  {path: 'privado',component: PrivadoComponent},
+  {path: '**',component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+ }
