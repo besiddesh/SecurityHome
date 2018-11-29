a0911ef53b3eed2c1a5494dcb713b04b64ca492a
Besiddes <Besiddes1@gmail.com>
2018-11-28 20:17:06 -0600

    Creacion de Componentes

--- SecurityHome/src/app/app.module.ts
+++ SecurityHome/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './componentes/home-page/home-page.component';
+import { NavBarComponent } from './componentes/nav-bar/nav-bar.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { PrivadoComponent } from './componentes/privado/privado.component';
+import { NotFoundComponent } from './componentes/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavBarComponent,
+    LoginComponent,
+    RegistroComponent,
+    PrivadoComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
